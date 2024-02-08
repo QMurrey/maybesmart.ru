555e99fa90dfa0061a0da2335b6b91503114ee16
QMurrey <quantmurrey@yandex.ru>
2024-02-08 11:48:40 +0600

    hotfix
    Таблица была доступна для изменения обычными пользователями. Сделал доступной только для администратора

--- assets/template-murrey-default/snippets/download_services_table.php
+++ assets/template-murrey-default/snippets/download_services_table.php
@@ -1,12 +1,13 @@
 <?php
 $sql = "SELECT * FROM maybe_smart_services";
+$editable = $modx->getOption('editable', $scriptProperties, '');
 foreach ($modx->query($sql) as $service) {
 	print "<tr id='service-".$service['id']."'>";
-		print "<td class='name' contenteditable='true' onBlur='updateDB(".$service['id'].")'>";
+		print "<td class='name' $editable onBlur='updateDB(".$service['id'].")'>";
 			print $service['name'] . '<br>';
-		print "</td><td class='price' contenteditable='true' onBlur='updateDB(".$service['id'].")'>";
+		print "</td><td class='price' $editable onBlur='updateDB(".$service['id'].")'>";
 			print $service['price'] . '<br>';
-		print "</td><td class='measure' contenteditable='true' onBlur='updateDB(".$service['id'].")'>";
+		print "</td><td class='measure' $editable onBlur='updateDB(".$service['id'].")'>";
 			print $service['measure'] . '<br>';
 		print "</td>";
 	print "</tr>";
